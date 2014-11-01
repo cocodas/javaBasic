81a1d3eac0ad41ebb66f39776708a86e25d445cf
Sung Kyu <sunq0011@gmail.com>
2014-11-01 17:04:16 +0900

    rolend

---
+++ src/standard_of_java/execise/ch6/ques5/Exercise6_5Main.java
@@ -0,0 +1,13 @@
+package standard_of_java.execise.ch6.ques5;
+
+public class Exercise6_5Main {
+
+	public static void main(String[] args) {
+		Student s = new Student("ȫ�浿", 1, 1, 100, 60, 76);
+		
+		System.out.println(s.info());
+		
+		
+	}
+
+}
